bddcd7ac211022f62c6146184be708adf2bee23f
mtalha789 <mt452395@gmail.com>
2024-05-24 09:34:15 +0500

    Adding Components

---
+++ food-store/src/components/index.js
@@ -0,0 +1,11 @@
+import Navbar from './Navbar'
+import ProductCard from './ProductCard'
+import LoginForm from './LoginForm'
+import ProductForm from './ProductForm'
+
+export {
+    Navbar,
+    ProductCard,
+    LoginForm,
+    ProductForm
+}
